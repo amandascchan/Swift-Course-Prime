9fa2ab80a6cee355981c748e1ab6f532aeb7e5c3
amandascchan <amandascchan@gmail.com>
2016-09-02 15:42:33 -0400

    tells you if a natural number is prime

--- isItPrime/ViewController.swift
+++ isItPrime/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+ 
+    @IBOutlet weak var resultsLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +24,62 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func buttonPressed(sender: AnyObject) {
+        
+        if let userEnteredStr = textField.text {
+            
+            let userEnteredInt = Int(userEnteredStr)
+            
+            if let resultInt = userEnteredInt {
+       
+                var i = 2
+                var isPrime = true
+                if resultInt > 0 {
+                    
+                    
+                    while i < resultInt {
+                        
+                        if resultInt % i == 0 {
+                            
+                            isPrime = false
+                            break
+                            
+                        }
+                        i += 1
+                        
+                    }
+                    
+                    if isPrime && resultInt != 1{
+                        resultsLabel.text = "\(resultInt) is prime!"
+                    }
+                    else if resultInt == 1 {
+                        resultsLabel.text = "1 by definition cannot be a prime. How can we prime factorize every natural number if 1 is considered a prime?"
+                       
+                    }
+                    else {
+                         resultsLabel.text = "\(resultInt) is not prime!"
+                    }
+                    
+                }
+                else {
+                    
+                    resultsLabel.text = "Please enter a positive whole number"
+                    
+                }
+                
+            }
+            else {
+                resultsLabel.text = "Please enter a positive whole number"
+            }
+            
+            
+        }
+      
+      
+        
+   
+        
+        
+    }
 }
 
